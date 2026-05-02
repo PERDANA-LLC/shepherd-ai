278ce91603b2750719da576a755758d6206c6baf
root <root@srv834739.hstgr.cloud>
2026-05-02 21:53:56 +0000

    fix: await auth() in Clerk middleware (v7 Promise return)

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    const { userId } = auth();
+    const { userId } = await auth();
     if (!userId) {
       const signInUrl = new URL("/sign-in", req.url);
       signInUrl.searchParams.set("redirect_url", req.url);
